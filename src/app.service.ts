4ca95fb6e541a00197a4e417a1c1552e8b7ec015
Unknown65182 <unknown65182@gmail.com>
2022-01-28 14:44:54 +0300

    development docker

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World, but bye!';
+    return 'Hello World, but bye, dude!!!!!';
   }
 }
